93a6e2509fb5d770ff2e63b394f7d9544b156202
Casper Rogild Storm <casper+github@rogildstorm.com>
2026-02-17 15:01:58 +0100

    first version of scripting

--- src/screen/dashboard.rs
+++ src/screen/dashboard.rs
@@ -93,6 +93,8 @@ pub enum Event {
     ImagePreview(PathBuf, url::Url),
     ToggleFullscreen,
     Remove(Server),
+    ToggleScript(String),
+    RefreshScripts,
 }
 
 impl Dashboard {
@@ -418,6 +420,9 @@ impl Dashboard {
                                 TokenPriority::User,
                             );
                         }
+                    }
+                    pane::Message::RefreshScripts => {
+                        return (Task::none(), Some(Event::RefreshScripts));
                     }
                     pane::Message::ContentResized(id, size) => {
                         if let Some(state) = self.panes.get_mut(window, id) {
@@ -1532,6 +1537,7 @@ impl Dashboard {
         &'a self,
         window: window::Id,
         clients: &'a client::Map,
+        script_manager: &'a data::scripts::Manager,
         config: &'a Config,
         theme: &'a Theme,
     ) -> Element<'a, Message> {
@@ -1557,6 +1563,7 @@ impl Dashboard {
                         false,
                         clients,
                         &self.file_transfers,
+                        script_manager,
                         &self.history,
                         &self.previews,
                         &self.side_menu,
@@ -1588,6 +1595,7 @@ impl Dashboard {
         &'a self,
         servers: &'a server::Map,
         clients: &'a client::Map,
+        script_manager: &'a data::scripts::Manager,
         version: &'a Version,
         config: &'a Config,
         theme: &'a Theme,
@@ -1611,6 +1619,7 @@ impl Dashboard {
                     maximized,
                     clients,
                     &self.file_transfers,
+                    script_manager,
                     &self.history,
                     &self.previews,
                     &self.side_menu,
@@ -2336,6 +2345,9 @@ impl Dashboard {
             }
             buffer::Event::OpenServer(server) => {
                 return (Task::none(), Some(Event::OpenServer(server)));
+            }
+            buffer::Event::ToggleScript(name) => {
+                return (Task::none(), Some(Event::ToggleScript(name)));
             }
         }
 
