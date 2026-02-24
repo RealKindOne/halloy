e70dce9b1e0fa75f866783aeff1e81140c8ce161
Casper Rogild Storm <casper+github@rogildstorm.com>
2026-02-24 15:24:06 +0100

    mark focused buffers as read on incoming messages

--- src/main.rs
+++ src/main.rs
@@ -178,6 +178,7 @@ struct Halloy {
     controllers: stream::Map,
     modal: Option<Modal>,
     main_window: Window,
+    focused_window: Option<window::Id>,
     pending_logs: Vec<data::log::Record>,
     notifications: Notifications,
 }
@@ -252,6 +253,7 @@ impl Halloy {
                 config,
                 modal: None,
                 main_window,
+                focused_window: None,
                 pending_logs: vec![],
                 notifications,
             },
@@ -723,6 +725,7 @@ impl Halloy {
                                 &mut self.servers,
                                 &mut self.controllers,
                                 &self.main_window,
+                                self.focused_window,
                             );
                         }
 
@@ -810,6 +813,7 @@ impl Halloy {
                                                 &mut self.servers,
                                                 &mut self.controllers,
                                                 &self.main_window,
+                                                self.focused_window,
                                             );
                                         }
 
@@ -838,6 +842,21 @@ impl Halloy {
                 Task::none()
             }
             Message::Window(id, event) => {
+                match &event {
+                    window::Event::Focused => {
+                        self.focused_window = Some(id);
+                    }
+                    window::Event::Unfocused => {
+                        if self.focused_window == Some(id) {
+                            self.focused_window = None;
+                        }
+                    }
+                    window::Event::Opened { .. }
+                    | window::Event::Moved(_)
+                    | window::Event::Resized(_)
+                    | window::Event::CloseRequested => {}
+                }
+
                 if id == self.main_window.id {
                     match event {
                         window::Event::Moved(position) => {
@@ -1207,6 +1226,7 @@ impl Halloy {
                 &mut self.servers,
                 &mut self.controllers,
                 &self.main_window,
+                self.focused_window,
             );
         }
 
@@ -1252,6 +1272,7 @@ fn handle_client_event(
     servers: &mut server::Map,
     controllers: &mut stream::Map,
     main_window: &Window,
+    focused_window: Option<window::Id>,
 ) {
     use data::client::Event;
 
@@ -1275,6 +1296,7 @@ fn handle_client_event(
                 config,
                 notifications,
                 main_window,
+                focused_window,
             );
         }
         Event::WithTarget(encoded, our_nick, target) => {
@@ -1546,10 +1568,11 @@ fn handle_priv_or_notice(
     notification_enabled: bool,
     dashboard: &mut screen::Dashboard,
     commands: &mut Vec<Task<Message>>,
-    clients: &data::client::Map,
+    clients: &mut data::client::Map,
     config: &Config,
     notifications: &mut Notifications,
     main_window: &Window,
+    focused_window: Option<window::Id>,
 ) {
     let Some((mut msg, highlight)) = create_message_with_highlight(
         server, encoded, our_nick, config, clients,
@@ -1573,6 +1596,9 @@ fn handle_priv_or_notice(
     let window = kind
         .as_ref()
         .and_then(|kind| dashboard.find_window_with_history(kind));
+    let should_mark_as_read = config.buffer.mark_as_read.on_message
+        && !msg.blocked
+        && msg.triggers_unread();
 
     if let Some(highlight) = highlight {
         handle_highlight(
@@ -1607,6 +1633,14 @@ fn handle_priv_or_notice(
             .record_message(server, msg, &config.buffer)
             .map(Message::Dashboard),
     );
+
+    if should_mark_as_read && let Some(kind) = kind {
+        dashboard.mark_as_read_if_focused_and_at_bottom(
+            &kind,
+            clients,
+            focused_window,
+        );
+    }
 }
 
 fn handle_highlight(
